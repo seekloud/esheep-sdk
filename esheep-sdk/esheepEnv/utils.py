6eb5847c46bb1dfda659b7948f810a51d07ed93a
dongruoyan <dongruoyan@neotel.com.cn>
2019-01-11 17:26:17 +0800

    grpc stream

--- esheep-sdk/esheepEnv/utils.py
+++ esheep-sdk/esheepEnv/utils.py
@@ -1,27 +1,11 @@
 import numpy as np
 
 
-def bytes_to_array(image_data):
+def to_np_array(image_data, dtype=np.int8):
     width = image_data.width
     height = image_data.height
     pixel_length = image_data.pixel_length
     data = image_data.data
     assert len(data) == width * height * pixel_length
-    a = np.zeros((height, width))
-    r = np.zeros((height, width))
-    g = np.zeros((height, width))
-    b = np.zeros((height, width))
-    for i in range(len(data)):
-        row = int(i / (width * pixel_length))
-        column = int(i % (width * pixel_length) / pixel_length)
-        attribute = i % pixel_length
-        if attribute == 0:
-            a[row, column] = data[i]
-        elif attribute == 1:
-            r[row, column] = data[i]
-        elif attribute == 2:
-            g[row, column] = data[i]
-        else:
-            b[row, column] = data[i]
-    return np.array((a, r, g, b))
+    return np.frombuffer(data, dtype=dtype).reshape((height, width, pixel_length))
 
